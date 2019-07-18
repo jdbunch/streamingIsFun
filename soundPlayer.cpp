4f767b6a3e97278eb5fc9c6fd4413033791c9163
John Bunch <bunc713@tamu.edu>
2019-07-18 16:48:12 -0500

    Basic Audio Streamer (Windows)
    -This maybe works? idk

---
+++ soundPlayer.cpp
@@ -0,0 +1,14 @@
+#include <Mmsystem.h>
+#include <mciapi.h>
+//these two headers are already included in the <Windows.h> header
+#pragma comment(lib, "Winmm.lib")
+
+int main() {
+
+   mciSendString("open \"*.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
+
+   mciSendString("play mp3 wait", NULL, 0, NULL);
+
+   return 0;
+}
+
